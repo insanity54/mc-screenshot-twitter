2ca2d2e21550da31da337abbacf6ca55c0331d6c
Chris Grimmett <chris@grimtech.net>
2015-11-05 01:49:39 -0800

    fix this issue

--- server/job.js
+++ server/job.js
@@ -7,16 +7,6 @@ nconf.file(path.join(__dirname, '..', 'config.json'));
 var redisOpts = nconf.get('redis_client_options');
 assert.isDefined(redisOpts, 'redis client options not defined in config.json');
 
-
-/**
- * job id created in redis (id = INCR mcsh:observer:queue:index)
- * observer screenshot job added to redis (RPUSH mcsh:observer:queue screenshot,{{id}})
- * screenshot message added to redis (SET mcsh:screenshot:{{id}}:message {{message}})
- * screenshot player added to redis  (SET mcsh:screenshot:{{id}}:player  {{player}})
- * screenshot time added to redis    (SET mcsh:screenshot:{{id}}:time    {{time}})
- * job notification published to observer channel in redis (PUBLISH observer job)
- */
- 
  
 var cleanUp = function cleanUp() {
   this.publisher.PUBLISH('observer', 'job');
@@ -35,40 +25,41 @@ var cleanUp = function cleanUp() {
  * @param {string} params - the parameters sent to the command
  */
 var create = function create(type, player, params) {
+  var self = this;
   assert.isString(type, 'job.create received first parameter that was not a string. its first param must be a string');
   assert.isString(player, 'job.create received second parameter that was not a string. its second param must be a string');
-  this.red = redis.createClient(redisOpts);
-  this.publisher = redis.createClient(redisOpts);
+  self.red = redis.createClient(redisOpts);
+  self.publisher = redis.createClient(redisOpts);
   
   
   // get job id from redis
-  this.red.INCR('mcsh:observer:queue:index', function(err, id) {
+  self.red.INCR('mcsh:observer:queue:index', function(err, id) {
     if (err) throw err;
     assert.isNumber(id, 'redis reply to INCR gave us something that was not a number.');
     console.log('redis index=' + id);
     
     // add job to redis
     // set requesting player
-    this.red.SET('mcsh:job:'+type+':'+id+':player', player, function(err, reply) {
+    self.red.SET('mcsh:job:'+type+':'+id+':player', player, function(err, reply) {
       assert.isNull(err, 'error entering requesting player in redis failed with error');
       assert.equal(reply, 'OK', 'entering requesting player in redis did not return OK');
     
       // set time
-      this.red.SET('mcsh:job:'+type+':'+id+':time', moment().valueOf(), function(err, reply) {
+      self.red.SET('mcsh:job:'+type+':'+id+':time', moment().valueOf(), function(err, reply) {
         assert.isNull(err, 'error entering time in redis');
         assert.equal(reply, 'OK', 'entering time in redis did not return OK');
         
         // set message (if there is one)
         if (typeof(params) !== 'undefined') {
-          this.red.SET('mcsh:job:screenshot:'+id+':message', params, function(err, reply) {
+          self.red.SET('mcsh:job:screenshot:'+id+':message', params, function(err, reply) {
             assert.isNull(err, 'error entering message in redis');
             assert.equal(reply, 'OK', 'entering message in redis did not return OK');
 
-            finalize.call(this);
+            finalize.call(self);
           });
         }
         else {
-          finalize.call(this);
+          finalize.call(self);
         }
       });
     });
